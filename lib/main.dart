f2365cf7c9d81ac47c54d810fc21091be5bbd973
ni892039-byte <ni892039@gmail.com>
2026-09-26 01:32:01 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -265,7 +265,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
         return AlertDialog(
           title: const Text('প্রোফাইল আপডেট করুন'),
           content: Column(
-            mainAxisSize: MyAxisSize.min,
+            mainAxisSize: MainAxisSize.min,
             children: [
               TextField(
                 controller: _nameController,
